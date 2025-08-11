1f68db6a7c118edab5552fd31274423b64ab4e87
Trinidad Perea <trinidadperea10@gmail.com>
2025-08-11 13:07:44 -0300

    Pruebas luego de 100 piezas

--- src/main.py
+++ src/main.py
@@ -8,9 +8,10 @@ def main():
     # algoritmos
     algoritmos = ["Hill Climbing", "Simulated Annealing", "Genetico"]
     resultados_totales = []
-    semillas = random.sample(range(0,1000000),10)
+    semillas = random.sample(range(0,1000000),1)
+
     for i in range(len(semillas)):
-        
+    
         for algoritmo in algoritmos:
             resultado = controlador(algoritmo, semillas[i])
             resultados_totales.append((algoritmo, i+1, resultado))
